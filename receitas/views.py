fad8c9498d5229cfb12690e825026dcb657eaa49
Robson Leal <devleal@hotmail.com>
2022-01-18 20:32:32 -0300

    Criando um banco de dados no Postgree e integrando com o projeto e criando models para as receitas

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1: 'Bolo de Chocolate',
+        2: 'Bolo de limão',
+        3: 'Suco detox',
+        4: 'Pudim'
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
