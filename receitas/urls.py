e41084e11e014b9c34dbd3f21ccb2e41ad1e978e
Robson Leal <devleal@hotmail.com>
2022-01-20 11:47:30 -0300

    receitas com fotos funcionais, campo de busca funcional

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:receita_id>', views.receita, name='receita')
+    path('<int:receita_id>', views.receita, name='receita'),
+    path('buscar', views.buscar, name='buscar')
 ]
