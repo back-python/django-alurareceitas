58060b2c1e118b62c6698f0cb39c275a8ddf2c2b
Robson Leal <devleal@hotmail.com>
2022-01-20 10:42:32 -0300

    Agora consigo fazer edições nas tabelas do admin, sem ter que acessar cada receita, também criei um campo não funcional para as imagens

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria')
+    list_display = ('id', 'nome_receita', 'categoria', 'publicada')
     list_display_links = ('id', 'nome_receita')
+    list_editable = ('publicada',)
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
     list_per_page = 10
